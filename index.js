0824c02f00deab87185f7e4b6f277fa15232e993
joshrabovsky <jarsky.75@gmail.com>
2024-03-03 18:49:41 -0500

    POST/api/post/

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ require("./routes/recipetag.routes.js")(app);
 require("./routes/tag.routes.js")(app);
 require("./routes/recommendations.routes.js")(app);
 require("./routes/connections.routes.js")(app);
+require("./routes/post.routes.js")(app)
 
 const PORT = process.env.NODE_PORT || 8080;
 
