2ecf1caee68085a23c947d4253ebd12c6603db61
joshrabovsky <jarsky.75@gmail.com>
2024-03-18 15:01:06 -0400

    Update query to get total likes

--- services/feed.services.js
+++ services/feed.services.js
@@ -4,11 +4,18 @@ class FeedService {
 
     async getFeed(user_id){
 
-        const query = "SELECT * FROM POST p " +
-        "JOIN RECIPE r ON p.recipe_id = r.id " +
-        "WHERE user_id IN " +
-            `(SELECT followed_user_id FROM follower WHERE follower_user_id = '${user_id}')` +
-            "ORDER BY upload_date_in_utc DESC"
+        const query = "SELECT * FROM RECIPE r "+
+        "JOIN ( " +
+            "SELECT p.*, COALESCE(lp.total_likes,0) FROM Post p " +
+            "LEFT JOIN ( " +
+                    "SELECT post_id, COUNT(*) as total_likes FROM liked_posts " +
+                    "WHERE is_liked = true " +
+                    "GROUP BY (post_id) " +
+                ") lp ON (p.id = lp.post_id)) " +
+            "p ON p.recipe_id = r.id " +
+        "WHERE user_id IN (SELECT followed_user_id FROM follower WHERE follower_user_id = " + 
+        `'${user_id}') `+
+        "ORDER BY upload_date_in_utc DESC "
         const result = await pool.query(query);
         return result.rows;
 
